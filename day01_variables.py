23d7703558c2fbf4548822bdefcf9baaab31168b
modukuriv <modukuri.vinodkumar26@gmail.com>
2026-04-10 07:01:17 -0400

    Day 01: Variables

---
+++ day01_variables.py
@@ -0,0 +1,6 @@
+#What is a variable?
+#A variable is a container for storing data values. It allows us to assign a name to a value and use that name to refer to the value throughout our code.
+#Creating a variable
+x = 5
+y = "Hello, World!"
+#In the above code, we have created two variables: 'x' which holds the integer value 5, and 'y' which holds the string value "Hello, World!".
